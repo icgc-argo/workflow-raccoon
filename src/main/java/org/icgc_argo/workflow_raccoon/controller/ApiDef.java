68910c4c2a2e5623f617bbf353dfb4325f0ac82f
jaserud <udjaser@gmail.com>
2021-08-31 10:18:51 -0400

    Initialize worklow-raccon service (#1)
    * initial commit
    
    * add swagger-ui
    
    * fix package name, add Jenkinsfile and Dockerfile
    
    * update readme and mvn-fmt
    
    * comment out provisions for now
    
    * add license
    
    * update description

---
+++ src/main/java/org/icgc_argo/workflow_raccoon/controller/ApiDef.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of he GNU Affero General Public License v3.0.
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.icgc_argo.workflow_raccoon.controller;
+
+import static org.icgc_argo.workflow_raccoon.configs.SwaggerConfig.RUN_TAG_NAME;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.icgc_argo.workflow_raccoon.model.ApiResponse;
+import org.icgc_argo.workflow_raccoon.model.DryRunResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import reactor.core.publisher.Mono;
+
+@Api(tags = RUN_TAG_NAME)
+public interface ApiDef {
+
+  @ApiOperation(
+      value = "Trigger Garbage Collection Asynchronously",
+      notes =
+          "Runs garbage collection asynchronous which will cause the cleanup of stuck workflows and kubernetes resources.",
+      response = ApiResponse.class)
+  @PostMapping(path = "/run", produces = APPLICATION_JSON_VALUE)
+  Mono<ApiResponse> run();
+
+  @ApiOperation(
+      value = "Trigger Garbage Collection Dry Run Synchronously",
+      notes = "Does a dry-run of garbage collection synchronously reporting what it would change.",
+      response = ApiResponse.class)
+  @PostMapping(path = "/dry-run", produces = APPLICATION_JSON_VALUE)
+  Mono<DryRunResponse> dryRun();
+}
